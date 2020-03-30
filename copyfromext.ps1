53ddb2b9cd3d3c1130561674b95aa6de1acb9e7b
Dante Sparda <poundplay@gmail.com>
2020-03-29 22:53:15 -0400

    Create copyfromext.ps1
    when ran it should run a copy of file from whatever external USB device is connected.

---
+++ copyfromext.ps1
@@ -0,0 +1,12 @@
+#Sparda
+
+#import date
+$todaysDate = Get-Date -format yyyyMMdd
+#define folder for loot
+$destinationFolder = "C:\Users\Sparda\Documents\loot\$todaysDate"
+#define external device
+$mydrive=(GWmi Win32_LogicalDisk | ?{$_.interfacetype -eq "USB"})
+#create storage location
+if (!(Test-Path -path $destinationFolder)) {New-Item $destinationFolder -Type Directory}
+#find external device and copy desired data 
+Get-ChildItem -Path $mydrive -Recurse -Include *.jpg,*.jpeg,*.png  | Copy-Item -Destination $destinationFolder -verbose
